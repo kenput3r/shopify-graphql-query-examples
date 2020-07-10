664ac1485b1c7d22be29744180a499f8d7bf3c5d
Kenneth Lyerly <kenput3r@gmail.com>
2020-07-10 12:44:29 -0700

    add Shopify Storefront Graphql API query example

---
+++ storefront/collectionByHandle.js
@@ -0,0 +1,92 @@
+/**
+ * @module collectionByHandle
+ * @param {string} handle - The handle of the collection to query
+ * @returns {string} query string
+ * @description - API Version 2020-07
+ * - use case: get enough collection info for a collection page
+ * with up to 24 products displayed
+ * @see {@link https://shopify.dev/docs/storefront-api/reference/queryroot?api[version]=2020-07 QueryRoot}
+ * @see {@link https://shopify.dev/docs/storefront-api/reference/object/collectionconnection?api[version]=2020-07 CollectionConnection}
+ */
+
+export const collectionByHandle = (handle) => `
+{
+  collectionByHandle(handle: ${handle}) {
+    description(truncateAt: 300)
+    descriptionHtml
+    image {
+      altText
+      originalSrc
+      transformedSrc(maxHeight: 1000, maxWidth: 2048, crop: CENTER)
+    }
+    title
+    products(first: 24) {
+      pageInfo {
+        hasNextPage
+      }
+      edges {
+        cursor
+        node {
+          availableForSale
+          compareAtPriceRange {
+            maxVariantPrice {
+              amount
+              currencyCode
+            }
+            minVariantPrice {
+              amount
+              currencyCode
+            }
+          }
+          handle
+          id
+          images(first: 1) {
+            edges {
+              node{
+                altText
+                transformedSrc(maxWidth: 500, maxHeight: 400, crop: CENTER)
+              }
+            }
+          }
+          priceRange {
+            maxVariantPrice {
+              amount
+              currencyCode
+            }
+            minVariantPrice {
+              amount
+              currencyCode
+            }
+          }
+          productType
+          tags
+          title
+          totalInventory
+          vendor
+          variants(first: 100) {
+            edges {
+              node {
+                availableForSale
+                compareAtPriceV2 {
+                  amount
+                  currencyCode
+                }
+                id
+                image {
+                  transformedSrc(maxWidth: 500, maxHeight: 400, crop: CENTER)
+                }
+                priceV2 {
+                  amount
+                  currencyCode
+                }
+                quantityAvailable
+                title
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
